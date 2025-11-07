1fe576d81b5810748e9a2b3bd8128295f0d85a67
khadija204 <xediceqasimova679@gmail.com>
2025-11-07 10:17:06 +0400

    Add files via upload

---
+++ djangotutorial/polls/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def _str_(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)   # <- bu sətrə diqqət
+    votes = models.IntegerField(default=0)
+
+    def _str_(self):
+        return self.choice_text
+'''from django.db import models
+
+
+class Question(models.Model):
+    # ...
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    # ...
+    def __str__(self):
+        return self.choice_text
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    # ...
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+# Create your models here.
+'''
